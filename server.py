a41f6d7612e00b0c555335bcd59e9d96396e56ca
Chris S <smithc4@appstate.edu>
2018-02-12 14:25:53 -0500

    Update server.py

--- server.py
+++ server.py
@@ -41,19 +41,7 @@ class AutoSysServer(BotPlugin):
             return "Permission denied!"
         return "Invalid. Please try again."
 
-from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 
-class ConfFlow(BotFlow):
-    """ Conversation flows related to polls"""
-
-    @botflow
-    def conf(self, flow: FlowRoot):
-        """ This is a flow that can set a guessing game."""
-        # setup Flow
-        dialogue = flow.connect('retrieve', auto_trigger=True)
-        conf = dialogue.connect('confirm')
-        conf.connect('confirm')
-        conf.connect(FLOW_END, predicate=lambda ctx: ctx['tries'] == 0)
 # Used to run commands in terminal and capture the result in string var.
 #with tempfile.TemporaryFile() as tempf:
 #    proc = subprocess.Popen(['ls','-l'], stdout=tempf)
