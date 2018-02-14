beb67914b0bd94d3399eb07e02f7916771424aa5
Chris S <smithc4@appstate.edu>
2018-02-14 08:37:12 -0500

    Update server.py

--- server.py
+++ server.py
@@ -27,29 +27,29 @@ class AutoSysServer(BotPlugin):
     @botcmd
     def retrieve(self, msg, args):
         """Ask for the log file from errbot"""
-        msg.ctx['permission'] = False
-        msg.ctx['command'] = "retrieve"
-        msg.ctx['args'] = args
+        self['permission'] = False
+        self['command'] = "retrieve"
+        self['args'] = args
         self.send(msg.frm, "OK to execute command " + msg.body + " [Y/N]?")
         
     @botmatch(r'^[a-zA-Z]$', flow_only=True)
     def confirm(self, msg, match):
         guess = match.string.lower()
         if guess == 'y':
-            msg.ctx['permission'] = True
+            self['permission'] = True
             self.send(msg.frm, "Permission granted.")
-            return getattr(self, msg.ctx['command'] + "2")(msg, msg.ctx['args'])
+            return getattr(self, self['command'] + "2")(msg, self['args'])
         else:
             return "Permission denied."
 
     def retrieve2(self, msg, args):
         """Get the log file from errbot"""
-        if msg.ctx['permission']:
+        if self['permission']:
             self.send(msg.frm, "You did the thing!")
     
     def server_active2(self, msg, args):
         """Get the log file from errbot"""
-        if msg.ctx['permission']:
+        if self['permission']:
             self.send(msg.frm, "You did the thing again!")
 
 # Used to run commands in terminal and capture the result in string var.
