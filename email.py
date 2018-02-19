d7ae4f8c947ea7e838568ca088ad7273cf44d372
Chris S <smithc4@appstate.edu>
2018-02-19 16:29:18 -0500

    Update email.py

--- email.py
+++ email.py
@@ -1,14 +1,14 @@
-from errbot import BotPlugin, botcmd, botmatch#, BotFlow
+from errbot import BotPlugin, botcmd, botmatch, BotFlow
 import subprocess, tempfile, re, time
 import smtplib
 from email.mime import multipart, text, base
 from email import encoders
-#from Flow import ConfFlow
+from Flow import ConfFlow
 
 class Email(BotPlugin):
     """Email plugin for Errbot"""
 
-    #flow = ConfFlow(BotFlow)
+    flow = ConfFlow(BotFlow)
     
     @botcmd
     def acceba(self, msg, args):
@@ -28,8 +28,8 @@ class Email(BotPlugin):
     def retrieve(self, msg, args):
         """Set up file transfer"""
         self['command'] = "retrieve"
-        #if self['command'] in flow.commands:
-        #    self.send(msg.frm, "Yup")
+        if self['command'] in flow.commands:
+            self.send(msg.frm, "Yup")
         prompt = self.preconfirm(msg, args)
         return prompt
         
