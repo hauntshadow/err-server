2a044d5763f065f5215425ca400e2e95fccff6c5
Chris S <smithc4@appstate.edu>
2018-02-19 15:52:10 -0500

    Update flow.py

--- flow.py
+++ flow.py
@@ -2,13 +2,14 @@ from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 
 class ConfFlow(BotFlow):
 
+    commands = ['retrieve']#open("/var/lib/err/plugins/hauntshadow/err-server/concomm.txt", 'r').read().splitlines()
+        
     @botflow
     def conf(self, flow: FlowRoot):
         """ This flow sets up confirmation with the users."""
         # List of flows, confirm dialogues, and commands that need confirmation
         flows = []
         con = []
-        commands = open("/var/lib/err/plugins/hauntshadow/err-server/concomm.txt", 'r').read().splitlines()
         for i in range(len(commands)):
             commands[i] = commands[i].split(':')[1]
             flows.append(flow.connect(commands[i], auto_trigger=True))
