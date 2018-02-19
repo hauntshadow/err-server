7f372c5c934d08094e3dd87fff0ae69a3ae20fa9
Chris S <smithc4@appstate.edu>
2018-02-19 14:28:00 -0500

    Update and rename server.py to email.py

--- email.py
+++ email.py
@@ -4,29 +4,8 @@ import smtplib
 from email.mime import multipart, text, base
 from email import encoders
 
-class AutoSysServer(BotPlugin):
-    """AutoSys server plugin for Errbot"""
-
-    @botcmd
-    def server_target(self, msg, args):
-        """Target server for jobs"""
-        self['target_server'] = args
-        self.target_server = args
-        #with open('/var/errbot/target_server', 'w+') as file:
-        #    proc = subprocess.Popen(['echo',args], stdout=file)
-        #    proc.wait()
-        #    file.seek(0)
-        #    target_server = str(target_server) + str(file.read())
-        return "Targeted server: " + self['target_server']
-    
-    @botcmd
-    def server_active(self, msg, args):
-        """Retrieve targeted server"""
-        self['command'] = "server_active"
-        #target_server = ""
-        #with open('/var/errbot/target_server', 'r') as file:
-        #    target_server = str(file.read())
-        return "Currently targeted server: " + self['target_server']
+class Email(BotPlugin):
+    """Email plugin for Errbot"""
 
     @botcmd
     def acceba(self, msg, args):
