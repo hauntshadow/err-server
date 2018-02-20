8493d7e96f7d7c1e1436ef2d120b53effefa1bab
Chris S <smithc4@appstate.edu>
2018-02-20 14:33:00 -0500

    Update emailflow.py

--- emailflow.py
+++ emailflow.py
@@ -1,20 +1,7 @@
 from errbot import botflow, FlowRoot, BotFlow, FLOW_END
 
 commands = ['retrieve']#open("/var/lib/err/plugins/hauntshadow/err-server/concomm.txt", 'r').read().splitlines()
-    
-@botflow
-def addconf(self, flow: FlowRoot):
-    """ This flow sets up confirmation with the users."""
-    # List of flows, confirm dialogues, and commands that need confirmation
-    flows = []
-    con = []
-    for i in range(len(commands)):
-        #commands[i] = commands[i].split(':')[1]
-        flows.append(flow.connect(commands[i], auto_trigger=True))
-        con.append(flows[i].connect('confirm'))
-        con[i].connect('confirm')
-        con[i].connect(FLOW_END, predicate=lambda ctx: ctx['tries'] == 0)
-    
+       
 class ConfFlow(BotFlow):
        
     @botflow
