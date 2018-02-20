f869521b9b4f3180e6498b506775a5d83de4cdac
Chris S <smithc4@appstate.edu>
2018-02-20 09:24:09 -0500

    Update email.py

--- email.py
+++ email.py
@@ -3,13 +3,13 @@ import subprocess, tempfile, re, time
 import smtplib
 from email.mime import multipart, text, base
 from email import encoders
-from flow import ConfFlow
+import flow
 
 class Email(BotPlugin):
     """Email plugin for Errbot"""
 
     
-    flowmod = ConfFlow(BotFlow(BotPlugin))
+    flowmod = flow.ConfFlow(BotFlow(BotPlugin))
     
     @botcmd
     def acceba(self, msg, args):
