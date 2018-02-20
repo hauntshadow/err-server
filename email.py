ab4ef7655f9c943569ec6fb53efbf6eb3f0db681
Chris S <smithc4@appstate.edu>
2018-02-20 15:14:32 -0500

    Update email.py

--- email.py
+++ email.py
@@ -18,7 +18,6 @@ class Email(BotPlugin):
         if (self['command'] in emailflow.commands and attempts == 0 and self['permission'] == True) or self['command'] not in emailflow.commands:
             if args not in emailflow.commands:
                 emailflow.commands.append(args)
-                emailflow.addconf()
                 return "Command " + args + " added to list of confirm commands."
             else:
                 return "Command already in list of confirm commands."
